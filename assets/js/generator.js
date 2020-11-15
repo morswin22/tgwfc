6a0634b9edb3349998ebe0aa6c255074c5012fe5
morswin22 <xmorswinx@gmail.com>
2020-11-15 17:09:26 +0100

    :sparkles: Add wave function collapse algorithm

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -21,6 +21,7 @@ function createGenerator() {
   let nSize;
   let width;
   let height;
+  let wave;
 
   const { on, emit } = createEvents();
 
@@ -31,6 +32,7 @@ function createGenerator() {
     const onReady = ready => {
       if (ready) {
         loader.off('loaded', onReady);
+        // TODO If nSize changed then resetWave
         nSize = loader.get(tiles[0]).width;
         updateInputs({ min: nSize*2, step: nSize, initial: nSize*5 });
       }
@@ -44,6 +46,7 @@ function createGenerator() {
 
   on('resized', () => width && height && resizeCanvas(width, height));
 
+  // TODO If width or height changed then resetWave
   const widthInput = createInputSlider('Width', nSize*2, windowWidth, nSize, nSize*5, value => emit('resized', width = Math.ceil(value/nSize)*nSize));
   const heightInput = createInputSlider('Height', nSize*2, windowHeight, nSize, nSize*5, value => emit('resized', height = Math.ceil(value/nSize)*nSize));
 
@@ -57,7 +60,84 @@ function createGenerator() {
       widthInput.updateConfiguration({ max: width });
       heightInput.updateConfiguration({ max: height });
     });
+    globalEvents.on('pressed', ([x, y]) => {
+      if (x > 0 && x < width && y > 0 && y < height) {
+        x = Math.floor(x / nSize);
+        y = Math.floor(y / nSize);
+        const picked = [random(tiles), random(tiles), random(tiles)];
+        for (const tile of tiles) {
+          if (picked.indexOf(tile) !== -1) {
+            wave[y][x][tile] = false;
+          }
+        }
+        console.log('changed at', x, 'x', y, 'to', picked);
+      }
+    });
   }
 
-  return { setConfiguration };
+  const resetWave = () => {
+    const h = Math.floor(height / nSize);
+    const w = Math.floor(width / nSize);
+    wave = [];
+    for (let i = 0; i < h; i++) {
+      wave[i] = [];
+      for (let j = 0; j < w; j++) {
+        wave[i][j] = Object.fromEntries(tiles.map(tile => [tile, true]));
+      }
+    }
+  };
+
+  const iterate = () => {
+    const [x, y] = getMinEntropy();
+    if (x !== undefined && y !== undefined) {
+      const collapsed = collapse(x, y);
+      propagate(x, y, collapsed);
+      emit('redraw', [wave, tiles]);
+    } else {
+      console.log('Algorithm: Contradictory state');
+    }
+  };
+
+  const getMinEntropy = () => {
+    let min = tiles.length;
+    let x, y;
+    for (let i = 0; i < wave.length; i++) {
+      for (let j = 0; j < wave[i].length; j++) {
+        const sum = Object.values(wave[i][j]).reduce((value, acc) => acc + value);
+        if (sum > 1 && min > sum) {
+          min = wave[i][j];
+          x = j;
+          y = i;
+        }
+      }
+    }
+    return [x, y];
+  }
+
+  const collapse = (x, y) => {
+    // TODO Use distributions for random picking tiles
+    const picked = random(Object.entries(wave[y][x]).filter(([, state]) => state).flatMap(([tile, ]) => tile));
+    for (const tile of tiles) {
+      if (tile !== picked) {
+        wave[y][x][tile] = false;
+      }
+    }
+    return picked;
+  }
+
+  const propagate = (x, y, collapsed) => {
+    // TODO This only propagates to nearest siblings but it needs to propagate further
+    const neighboursPossibleStates = Object.values(configuration[collapsed]);
+    const neighboursPositions = [[y-1, x], [y, x+1], [y+1, x], [y, x-1], ];
+    for (let i = 0; i < neighboursPositions.length; i++) {
+      const [ny, nx] = neighboursPositions[i];
+      if (ny >= 0 && ny < wave.length && nx >= 0 && nx < wave[ny].length) {
+        for (const tile in wave[ny][nx]) {
+          if (neighboursPossibleStates[i][tile] === false) wave[ny][nx][tile] = false; 
+        }
+      }
+    }
+  }
+
+  return { setConfiguration, resetWave, iterate, on };
 }
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -2,21 +2,38 @@ const globalEvents = createEvents();
 
 function setup() {
   createCanvas(400, 400);
+  background(51);
 
   const generator = createGenerator();
   const { fromPreset, configuration } = createConfigurator();
   createPresets('/assets/presets/presets.json', fromPreset);
 
-  createButton('Use loaded').mousePressed(() => {
-    generator.setConfiguration(configuration);
-  });
+  createButton('Use loaded').mousePressed(() => generator.setConfiguration(configuration));
 
+  createButton('Reset wave').mousePressed(generator.resetWave);
+  createButton('Start collapsing').mousePressed(generator.iterate);
+
+  generator.on('redraw', ([wave, tiles]) => {
+    for (let i = 0; i < wave.length; i++) {
+      for (let j = 0; j < wave[i].length; j++) {
+        const possible = Object.entries(wave[i][j]).filter(([, state]) => state).flatMap(([tile, ]) => tile)
+        if (possible.length === tiles.length) continue;
+        const opacity = 1 - possible.length / tiles.length;
+        tint(255, opacity * 255);
+        for (const tile of possible) {
+          const img = loader.get(tile);
+          image(img, img.width * j, img.height * i);
+        }
+      }
+    }
+    setTimeout(generator.iterate, 0);
+  });
 }
 
 function windowResized() {
   globalEvents.emit('resized', [windowWidth, windowHeight]);
 }
 
-function draw() {
-  background(51);
+function mousePressed() {
+  globalEvents.emit('pressed', [mouseX, mouseY]);
 }
